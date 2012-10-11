d5d301eb2800103cd469eb29fe139a7aa9ca41f5
Robin Chew <me@robin.com.au>
2012-10-12 02:28:58 +0800

    Create QR code and center it and the texts.

--- card.py
+++ card.py
@@ -5,6 +5,10 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase.pdfmetrics import getFont,stringWidth,registerFont
 from reportlab.graphics.charts.textlabels import _text2Path
 
+from svglib import svg2rlg
+import qrcode
+from qrcode.image.svg import SvgImage
+
 registerFont(TTFont('OpenSans','/home/robin/.fonts/opensans/OpenSans-Regular-webfont.ttf'))
 registerFont(TTFont('OpenSansBold','/home/robin/.fonts/opensans/OpenSans-Bold-webfont.ttf'))
 
@@ -18,21 +22,21 @@ grey   = PCMYKColor( 0.0, 0.0, 0.0, 70 )
 class Text(object):
     font = 'OpenSans'
 
-    def height_of(self,s):
-        x1,y1,x2,y2 = _text2Path(s,fontName=self.font,fontSize=1).getBounds()
-        #print 'xyxy',x1,y1,x2,y2
-        return (y2+y1/5)+0.3
-        
-    def __init__(self,text,font=None,colour=None):
+    def __init__(self,text,font=None,colour=None,x=0):
         font = font if font else self.font
         self.font = font
-        box_w = 40*PT_PER_MM
+        self.width = 40*PT_PER_MM
         self.text = text
-        self.width = stringWidth(text,font, 1000)
-        self.font_size = self.scale = box_w/(self.width/1000)
+        str_width = stringWidth(text,font, 1000)
+        self.font_size = self.scale = self.width/(str_width/1000)
         self.height = self.height_of(text) * self.scale
         self.colour = colour if colour else black
+        self.x = x
 
+    def height_of(self,s):
+        x1,y1,x2,y2 = _text2Path(s,fontName=self.font,fontSize=1).getBounds()
+        return (y2+y1/5)+0.3
+        
     def __str__(self):
         return self.text
 
@@ -48,14 +52,23 @@ class Card(object):
     def text(self,text):
         self.text_list.append(text)
 
+    @property
+    def total_text_height(self):
+        return sum(text.height for text in self.text_list)
+
     def draw(self):
         ctx = self.ctx
-        box_h = self.height
+        box_h = self.total_text_height
+
         for text in self.text_list:
             ctx.setFillColor(text.colour)
             ctx.setFont(text.font,text.font_size)
-            ctx.drawString(0,box_h-text.height,str(text))
+            ctx.drawString(text.x,box_h-text.height,str(text))
             box_h -= text.height
+
+    def move_texts(self,x):
+        for text in self.text_list:
+            text.x = x
 
     def move(self,x,y):
         self.ctx.translate(x,y)
@@ -68,13 +81,25 @@ card.text(Text('Paul Cartwright','OpenSansBold'))
 card.text(Text('Software Engineer'))
 card.text(Text('paul@obsi.com.au',colour=grey))
 card.text(Text('0403 048 754',colour=grey))
+url_name = 'ploy'
 
-total_text_h = sum(text.height for text in card.text_list)
+qr = qrcode.QRCode(border=0)
+qr.add_data('http://obsi.com.au/%s/' % url_name)
+qr.make(fit=True)
+img = qr.make_image(image_factory=SvgImage)
+img.save('qr.svg')
 
-card.move(card.width/2-20*PT_PER_MM,-(card.height/2-total_text_h/2))
-print card.width/2-20*PT_PER_MM,-(card.height/2-total_text_h/2)
-print 'th',total_text_h
+qr_width = 25*PT_PER_MM
+qr_scale = card.total_text_height/qr_width
+qr_width = qr_width * qr_scale
+card.move_texts(qr_width+2*PT_PER_MM)
+drawing = svg2rlg('qr.svg')
+
+card.move(card.width/2-(qr_width+2*PT_PER_MM+card.text_list[0].width)/2,card.height/2-card.total_text_height/2)
 card.draw()
+card.ctx.scale(qr_scale,qr_scale)
+drawing.drawOn(card.ctx,0,0)
+
 card.save()
 
 """
